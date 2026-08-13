9617b5a6c9d621a7bbb4ee15bbc046b2284fbb73
Luiz Henrique Cruz Guerra <luizguerra.dev@gmail.com>
2026-08-13 19:49:26 -0300

    Configurar classe CLIENTE | ISSUE #2

--- src/main/java/dev/java10x/CadastroDeClientes/ClienteModel.java
+++ src/main/java/dev/java10x/CadastroDeClientes/ClienteModel.java
@@ -4,17 +4,17 @@ import jakarta.persistence.*;
 
 
 // Entity ele transforma uma classe em uma entidade do BD
-// JPA - Java Persistence API
+// JPA - Java Persistence api
 @Entity
 @Table(name = "tb_cadastro")
 public class ClienteModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
-    String nome;
-    String email;
-    int idade;
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
 
  public ClienteModel(){
  }
@@ -24,5 +24,26 @@ public class ClienteModel {
     this.email = email;
     this.idade = idade;
  }
+ public String getNome() {
+     return nome;
+ }
 
+ public void setNome(String nome) {
+     this.nome = nome;
+ }
+
+ public String getEmail() {
+     return email;
+ }
+
+ public void setEmail(String email) {
+     this.email = email;
+ }
+
+ public int getIdade() {
+     return idade;
+ }
+ public void setIdade(int idade) {
+     this.idade = idade;
+ }
 }
